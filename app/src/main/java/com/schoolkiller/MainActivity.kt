1c955469a676609688992058e3f0782533835e05
Beatoriche-sama <beatokaine@gmail.com>
2024-09-25 20:47:43 +0800

    Advertisement branch.

--- app/src/main/java/com/schoolkiller/MainActivity.kt
+++ app/src/main/java/com/schoolkiller/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.schoolkiller
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,14 +9,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.ads.MobileAds
 import com.schoolkiller.ui.navigation.NavigationController
 import com.schoolkiller.ui.theme.SchoolKillerTheme
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    //@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private lateinit var appOpenAdManager: AppOpenAdManager
+
+    @RequiresApi(value = 34)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,5 +35,15 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+        MobileAds.initialize(this) {
+            Timber.tag("tag").d("Initializing MobileAds")
+        }
+        appOpenAdManager = AppOpenAdManager()
     }
+
+    override fun onStart() {
+        super.onStart()
+        appOpenAdManager.fetchAd(this)
+    }
+
 }
--- app/src/main/java/com/schoolkiller/SchoolKillerApplication.kt
+++ app/src/main/java/com/schoolkiller/SchoolKillerApplication.kt
@@ -1,27 +1,8 @@
 package com.schoolkiller
 
 import android.app.Application
-import android.util.Log
-import androidx.lifecycle.ProcessLifecycleOwner
-import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SchoolKillerApplication : Application() {
-    private lateinit var appOpenAdManager: AppOpenAdManager
-
-    override fun onCreate() {
-        super.onCreate()
-        MobileAds.initialize(this) {
-            Log.d("tag", "MobileAds init ")
-        }
-        appOpenAdManager = AppOpenAdManager(this)
-
-        ProcessLifecycleOwner.get().lifecycle.addObserver(
-            appOpenAdManager.defaultLifecycleObserver
-        )
-
-    }
-
-}
+class SchoolKillerApplication : Application()
 
