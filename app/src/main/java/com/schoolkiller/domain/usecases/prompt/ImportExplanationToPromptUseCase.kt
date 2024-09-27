34b3e003d760a39c38865ae87a3dd8c4563de2fe
Beatoriche-sama <beatokaine@gmail.com>
2024-09-27 10:31:07 +0800

    Translation issue was fixed, now using codes instead of string text for prompt's imports. Prompt original string is untranslatable. Changed additional information textfield text colours (black and gray) to material theme colours

--- app/src/main/java/com/schoolkiller/domain/usecases/prompt/ImportExplanationToPromptUseCase.kt
+++ app/src/main/java/com/schoolkiller/domain/usecases/prompt/ImportExplanationToPromptUseCase.kt
@@ -1,23 +1,15 @@
 package com.schoolkiller.domain.usecases.prompt
 
-import android.content.Context
-import com.schoolkiller.R
 import com.schoolkiller.domain.ExplanationLevelOptions
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class ImportExplanationToPromptUseCase @Inject constructor(
-@ApplicationContext
-private val context: Context,
-) {
-    private val explanationArray: Array<String> = context.resources.getStringArray(R.array.explanations)
+class ImportExplanationToPromptUseCase @Inject constructor() {
 
     fun invoke(
         explanationOption: ExplanationLevelOptions,
         originalPrompt: String
     ): String {
-        val explanationString = explanationArray.getOrNull(explanationOption.arrayIndex)
-            ?: "" // Handle potential out-of-bounds access
-        return originalPrompt.replace("(briefly)", " in $explanationString")
+        return originalPrompt.replace("(briefly)",
+            " ${explanationOption.code}")
     }
 }
